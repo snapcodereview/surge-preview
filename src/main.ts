0213610b6a44620fe7bc2c5d7dfa3799f8eedd5b
Uttam Kini <uttamkini@gmail.com>
2021-07-24 21:22:03 +0530

    Use only repo name in preview url

--- src/main.ts
+++ src/main.ts
@@ -93,7 +93,7 @@ ${getCommentFooter()}
 
   const repoOwner = github.context.repo.owner.replace(/\./g, '-');
   const repoName = github.context.repo.repo.replace(/\./g, '-');
-  const url = `${repoOwner}-${repoName}-${job}-pr-${prNumber}.surge.sh`;
+  const url = `${repoName}-pr-${prNumber}.surge.sh`;
 
   core.setOutput('preview_url', url);
 
